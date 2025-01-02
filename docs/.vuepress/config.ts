c5e281f77ad02d0bdc84cb0d43709e72f691348c
Henry Thomas <henrythomas9587@gmail.com>
2025-01-03 01:27:51 +0800

    feat: 添加工作部署文件

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,7 +5,7 @@ import { join } from "path";
 
 export default defineUserConfig({
   title: "My Documentation Site",
-  base: "/app/", // 修改这里
+  base: "/", // 修改这里
   description: "VuePress Starter Project",
 
   bundler: viteBundler({
